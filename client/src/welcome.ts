826bb8142d09d7596fd0438af40b1a34a871aa62
kalish <uri.kalish@gmail.com>
2022-10-17 03:41:24 +0300

    options on welcome screen

--- client/src/welcome.ts
+++ client/src/welcome.ts
@@ -5,32 +5,59 @@ import { UIHelper } from './ui-helper.js';
 export class Welcome {
 	static init(onWelcomeDone) {
 		const fenTextElm = UIHelper.getElm('welcome-fen-text');
-		const playerNameTextElm = UIHelper.getElm('welcome-player-name-text');
-		const playerWhiteElm = UIHelper.getElm('welcome-player-color-selector-white');
-		const playerBlackElm = UIHelper.getElm('welcome-player-color-selector-black');
-		const startButtonElm = UIHelper.getElm('welcome-start-button');
 
-		if (!fenTextElm || !playerNameTextElm || !playerWhiteElm || !playerBlackElm || !startButtonElm) {
+		const hvhWhiteNameElm = UIHelper.getElm('welcome-hvh-white-name-text');
+		const hvhBlackNameElm = UIHelper.getElm('welcome-hvh-black-name-text');
+		const hvhStartButtonElm = UIHelper.getElm('welcome-hvh-start-button');
+
+		const hvmPlayerNameElm = UIHelper.getElm('welcome-hvm-player-name-text');
+		const hvmPlayerIsWhiteElm = UIHelper.getElm('welcome-hvm-player-color-selector-white');
+		const hvmPlayerIsBlackElm = UIHelper.getElm('welcome-hvm-player-color-selector-black');
+		const hvmStartButtonElm = UIHelper.getElm('welcome-hvm-start-button');
+
+		const mvmStartButtonElm = UIHelper.getElm('welcome-mvm-start-button');
+
+		if (
+			!fenTextElm ||
+			!hvhWhiteNameElm ||
+			!hvhBlackNameElm ||
+			!hvhStartButtonElm ||
+			!hvmPlayerNameElm ||
+			!hvmPlayerIsWhiteElm ||
+			!hvmPlayerIsBlackElm ||
+			!hvmStartButtonElm ||
+			!mvmStartButtonElm
+		) {
 			return;
 		}
 		(fenTextElm as HTMLInputElement).value = Fen.default;
-		playerWhiteElm.addEventListener('click', () => {
-			playerWhiteElm.classList.add('selected');
-			playerBlackElm.classList.remove('selected');
-		});
-		playerBlackElm.addEventListener('click', () => {
-			playerBlackElm.classList.add('selected');
-			playerWhiteElm.classList.remove('selected');
-		});
-		startButtonElm.addEventListener('click', () => {
+		hvhStartButtonElm.addEventListener('click', () => {
 			const fenStr = (fenTextElm as HTMLInputElement).value || Fen.default;
-			const playerName = (playerNameTextElm as HTMLInputElement).value || 'Player1';
-			const isPlayerWhite = playerWhiteElm.classList.contains('selected');
+			const whitePlayerName = (hvhWhiteNameElm as HTMLInputElement).value || 'Player1';
+			const blackPlayerName = (hvhBlackNameElm as HTMLInputElement).value || 'Player2';
+			onWelcomeDone(fenStr, PlayerType.HUMAN, whitePlayerName, PlayerType.HUMAN, blackPlayerName);
+		});
+		hvmPlayerIsWhiteElm.addEventListener('click', () => {
+			hvmPlayerIsWhiteElm.classList.add('selected');
+			hvmPlayerIsBlackElm.classList.remove('selected');
+		});
+		hvmPlayerIsBlackElm.addEventListener('click', () => {
+			hvmPlayerIsBlackElm.classList.add('selected');
+			hvmPlayerIsWhiteElm.classList.remove('selected');
+		});
+		hvmStartButtonElm.addEventListener('click', () => {
+			const fenStr = (fenTextElm as HTMLInputElement).value || Fen.default;
+			const playerName = (hvmPlayerNameElm as HTMLInputElement).value || 'Player1';
+			const isPlayerWhite = hvmPlayerIsWhiteElm.classList.contains('selected');
 			if (isPlayerWhite) {
 				onWelcomeDone(fenStr, PlayerType.HUMAN, playerName, PlayerType.COMPUTER, 'Computer');
 			} else {
 				onWelcomeDone(fenStr, PlayerType.COMPUTER, 'Computer', PlayerType.HUMAN, playerName);
 			}
+		});
+		mvmStartButtonElm.addEventListener('click', () => {
+			const fenStr = (fenTextElm as HTMLInputElement).value || Fen.default;
+			onWelcomeDone(fenStr, PlayerType.COMPUTER, 'Computer1', PlayerType.COMPUTER, 'Computer2');
 		});
 	}
 }
