8574138ec27eaeec70b9b9c70a3a124a3fb2a337
kalish <uri.kalish@gmail.com>
2022-11-03 20:23:53 +0200

    log first move for active black fen

--- client/src/ui/ui-log.ts
+++ client/src/ui/ui-log.ts
@@ -1,6 +1,7 @@
 import { Helper } from '../helper';
 import { Move } from '../move';
 import { UiHelper } from './ui-helper';
+import { MoveType } from '../types';
 
 export class UiLog {
 	static startTime = 0;
@@ -9,16 +10,34 @@ export class UiLog {
 		return `${Helper.getTimeStr(new Date().getTime() - UiLog.startTime)}`;
 	}
 
-	static createMoveElm(move) {
-		const movePartClass = 'info-log--move-part';
-		const moveNameClass = 'info-log--move-name';
-		const moveTypeClassPrefix = 'info-log--move-type-';
+	static classNames = {
+		fullMove: 'info-log--full-move',
+		movePart: 'info-log--move-part',
+		moveNumber: 'info-log--move-number',
+		moveName: 'info-log--move-name',
+		moveTypePrefix: 'info-log--move-type-',
+	};
+
+	static createFullMoveElm() {
+		const elm: HTMLDivElement = document.createElement('div');
+		elm.classList.add(UiLog.classNames.fullMove);
+		return elm;
+	}
+
+	static createMoveNumberElm(fullMoveNumber: number) {
 		const elm = document.createElement('span');
-		elm.innerText = move.name;
+		elm.innerText = `${String(fullMoveNumber)})`;
+		elm.classList.add(UiLog.classNames.movePart, UiLog.classNames.moveNumber);
+		return elm;
+	}
+
+	static createMoveElm(moveName: string, moveTypes: Set<MoveType>) {
+		const elm = document.createElement('span');
+		elm.innerText = moveName;
 		elm.title = UiLog.getTimeStr();
-		elm.classList.add(movePartClass, moveNameClass);
-		move.types.forEach(t => {
-			elm.classList.add(`${moveTypeClassPrefix}${t}`);
+		elm.classList.add(UiLog.classNames.movePart, UiLog.classNames.moveName);
+		moveTypes.forEach(t => {
+			elm.classList.add(`${UiLog.classNames.moveTypePrefix}${t}`);
 		});
 		return elm;
 	}
@@ -28,28 +47,27 @@ export class UiLog {
 		if (!panelElm) {
 			return;
 		}
-		const fullMoveClass = 'info-log--full-move';
-		const movePartClass = 'info-log--move-part';
-		const moveNumberClass = 'info-log--move-number';
 		if (move.armyIndex === 0) {
-			const fullMoveElm: HTMLDivElement = document.createElement('div');
-			fullMoveElm.classList.add(fullMoveClass);
-
-			const moveNumElm = document.createElement('span');
-			moveNumElm.innerText = `${String(move.fullMoveNumber)})`;
-			moveNumElm.classList.add(movePartClass, moveNumberClass);
+			const fullMoveElm = UiLog.createFullMoveElm();
+			const moveNumElm = UiLog.createMoveNumberElm(move.fullMoveNumber);
+			const whiteMoveElm = UiLog.createMoveElm(move.name, move.types);
 			fullMoveElm.appendChild(moveNumElm);
-
-			const whiteMoveElm = UiLog.createMoveElm(move);
 			fullMoveElm.appendChild(whiteMoveElm);
-
 			panelElm.appendChild(fullMoveElm);
 		} else {
-			const fullMoveElms = panelElm.querySelectorAll(`.${fullMoveClass}`);
-			const fullMoveElm = fullMoveElms[fullMoveElms.length - 1];
-
-			const blackMoveElm = UiLog.createMoveElm(move);
-
+			let fullMoveElm;
+			const fullMoveElms = panelElm.querySelectorAll(`.${UiLog.classNames.fullMove}`);
+			if (fullMoveElms.length > 0) {
+				fullMoveElm = fullMoveElms[fullMoveElms.length - 1];
+			} else {
+				fullMoveElm = UiLog.createFullMoveElm();
+				const moveNumElm = UiLog.createMoveNumberElm(move.fullMoveNumber);
+				const whiteMoveElm = UiLog.createMoveElm('...', new Set([]));
+				fullMoveElm.appendChild(moveNumElm);
+				fullMoveElm.appendChild(whiteMoveElm);
+				panelElm.appendChild(fullMoveElm);
+			}
+			const blackMoveElm = UiLog.createMoveElm(move.name, move.types);
 			fullMoveElm.appendChild(blackMoveElm);
 		}
 		panelElm.scrollTo(0, panelElm.scrollHeight);
