329c4e17f1013580c1e87e45bec5ebed701934ad
kalish <uri.kalish@gmail.com>
2022-11-05 21:06:25 +0200

    position epTargetIndex

--- client/src/fen.ts
+++ client/src/fen.ts
@@ -1,4 +1,5 @@
 import { Position } from './position';
+import { Square } from './square';
 
 export class Fen {
 	static default = `rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1`;
@@ -16,7 +17,8 @@ export class Fen {
 				}
 			}
 		});
-		return new Position(pd, parts[1] === 'w' ? 0 : 1, parts[3], Number(parts[4]), Number(parts[5]));
+		const epTargetSquare = Square.getIndexByName(parts[3]);
+		return new Position(pd, parts[1] === 'w' ? 0 : 1, epTargetSquare, Number(parts[4]), Number(parts[5]));
 	}
 
 	static getFenStr(position: Position | null) {
@@ -49,7 +51,7 @@ export class Fen {
 		parts[0] = pd.join('');
 		parts[1] = ['w', 'b'][position.armyIndex];
 		parts[2] = 'KQkq';
-		parts[3] = position.epTargetSquare || '-';
+		parts[3] = position.epTargetIndex === -1 ? '' : Square.getNameByIndex(position.epTargetIndex);
 		parts[4] = String(position.halfMoveClock);
 		parts[5] = String(position.fullMoveNum);
 		return parts.join(' ');
--- client/src/position.ts
+++ client/src/position.ts
@@ -3,19 +3,19 @@ import { Helper } from './helper';
 export class Position {
 	pieceData: string[] = [];
 	armyIndex = 0;
-	epTargetSquare = '';
+	epTargetIndex = -1;
 	halfMoveClock = 0;
 	fullMoveNum = 1;
 
-	constructor(pieceData: string[], armyIndex: number, epTargetSquare: string, halfMoveClock: number, fullMoveNum: number) {
+	constructor(pieceData: string[], armyIndex: number, epTargetIndex: number, halfMoveClock: number, fullMoveNum: number) {
 		this.fullMoveNum = fullMoveNum;
 		this.armyIndex = armyIndex;
-		this.epTargetSquare = epTargetSquare;
+		this.epTargetIndex = epTargetIndex;
 		this.pieceData = pieceData;
 		this.halfMoveClock = halfMoveClock;
 	}
 
 	createNextPosition() {
-		return new Position([...this.pieceData], Helper.flipArmyIndex(this.armyIndex), '', this.halfMoveClock + 1, this.armyIndex === 0 ? this.fullMoveNum : this.fullMoveNum + 1);
+		return new Position([...this.pieceData], Helper.flipArmyIndex(this.armyIndex), -1, this.halfMoveClock + 1, this.armyIndex === 0 ? this.fullMoveNum : this.fullMoveNum + 1);
 	}
 }
