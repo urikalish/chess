4e64efcb946bfac37b3b9144a272664d6cc821c3
kalish <uri.kalish@gmail.com>
2022-11-11 17:37:12 +0200

    mobile style

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,13 @@ import { UiMain } from './ui/ui-main';
 let game: Game | null = null;
 let uiMain: UiMain | null = null;
 
+function setDocHeight() {
+	document.documentElement.style.setProperty('--doc-height', `${window.innerHeight}px`);
+}
+
 function init() {
+	window.addEventListener('resize', setDocHeight);
+	setDocHeight();
 	UiWelcome.showDialog((fenStr: string, player0Type, player0Name, player1Type, player1Name) => {
 		const startTime = new Date().getTime();
 		UiLog.startTime = startTime;
