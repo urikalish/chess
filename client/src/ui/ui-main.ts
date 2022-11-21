4b2b505088b629fd190128c8ab61704ad139f4d2
kalish <uri.kalish@gmail.com>
2022-11-21 23:54:30 +0200

    player progress fades in and out

--- client/src/ui/ui-main.ts
+++ client/src/ui/ui-main.ts
@@ -145,8 +145,8 @@ export class UiMain {
 		const playerProgressElms = UiHelper.queryElms('.player-status-progress');
 		const isActivePlayerBot = this.game.getCurPlayer()?.type === PlayerType.BOT;
 		this.setBotComputeProgress(0);
-		playerProgressElms[0].classList.toggle('hidden', !topActive || !isActivePlayerBot);
-		playerProgressElms[1].classList.toggle('hidden', !bottomActive || !isActivePlayerBot);
+		playerProgressElms[0].classList.toggle('transparent', !topActive || !isActivePlayerBot);
+		playerProgressElms[1].classList.toggle('transparent', !bottomActive || !isActivePlayerBot);
 	}
 
 	updateUI() {
