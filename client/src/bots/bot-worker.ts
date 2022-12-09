78b63b9c6184109de8472cadff3a9d88c91ee603
kalish <uri.kalish@gmail.com>
2022-12-09 15:49:15 +0200

    bot creation

--- client/src/bots/bot-worker.ts
+++ client/src/bots/bot-worker.ts
@@ -1,17 +1,11 @@
 import { Position } from '../model/position';
-import { Move } from '../model/move';
 import { Bot } from './bot';
 
 export class BotWorker {
-	static bots = {
-		bot1: new Bot(1, true, BotWorker.handleBotProgress),
-		bot2: new Bot(2, true, BotWorker.handleBotProgress),
-		bot3: new Bot(3, true, BotWorker.handleBotProgress),
-		bot4: new Bot(4, true, BotWorker.handleBotProgress),
-	};
-
-	static goComputeMove(botName: string, p: Position, moveNames: string[]): Move | null {
-		return BotWorker.bots[botName].goComputeMove(p, moveNames);
+	static goComputeMove(botName: string, p: Position, moveNames: string[]) {
+		const depth = Number(botName.substring(3));
+		const bot = new Bot(depth, true, BotWorker.handleBotProgress);
+		bot.goComputeMove(p, moveNames);
 	}
 
 	static handleBotProgress(progress: number, moveName: string) {
